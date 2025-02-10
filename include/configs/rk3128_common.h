67120a507082d9d0db569a2ac55303dcb1386f3c
Alex Bee <knaerzche@gmail.com>
2025-02-10 14:21:55 +0100

    rockchip: rk3128: Align environment variables
    - align address with other SoCs
    - support ROCKCHIP_DEVICE_SETTINGS
    - support fdt overlays
    - add kernel_comp_* variables
    - only add in non-xpl builds
    
    Signed-off-by: Alex Bee <knaerzche@gmail.com>

--- include/configs/rk3128_common.h
+++ include/configs/rk3128_common.h
@@ -15,12 +15,23 @@
 #define CFG_SYS_SDRAM_BASE		0x60000000
 #define SDRAM_MAX_SIZE			0x80000000
 
+#ifndef CONFIG_XPL_BUILD
+
+#ifndef ROCKCHIP_DEVICE_SETTINGS
+#define ROCKCHIP_DEVICE_SETTINGS
+#endif
+
 #define ENV_MEM_LAYOUT_SETTINGS \
 	"scriptaddr=0x60500000\0" \
+	"script_offset_f=0xffe000\0" \
+	"script_size_f=0x2000\0" \
 	"pxefile_addr_r=0x60600000\0" \
-	"fdt_addr_r=0x61f00000\0" \
-	"kernel_addr_r=0x62000000\0" \
-	"ramdisk_addr_r=0x64000000\0"
+	"fdt_addr_r=0x61e00000\0" \
+	"fdtoverlay_addr_r=0x61f00000\0" \
+	"kernel_addr_r=0x62080000\0" \
+	"ramdisk_addr_r=0x66000000\0" \
+	"kernel_comp_addr_r=0x68000000\0" \
+	"kernel_comp_size=0x2000000\0"
 
 #define CFG_EXTRA_ENV_SETTINGS \
 	ENV_MEM_LAYOUT_SETTINGS \
@@ -28,6 +39,9 @@
 	"fdt_high=0x7fffffff\0" \
 	"initrd_high=0x7fffffff\0" \
 	"partitions=" PARTS_DEFAULT \
+	ROCKCHIP_DEVICE_SETTINGS \
 	"boot_targets=" BOOT_TARGETS "\0"
 
-#endif
+#endif  /* CONFIG_XPL_BUILD */
+
+#endif /* __CONFIG_RK3128_COMMON_H */
